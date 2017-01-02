357abebb6f7c738f1fdbcb9a8bad8446d1a79dc3
Alexander Azarov <azarov@osinka.ru>
2017-01-02 22:04:17 +0200

    Refactoring to destructure code, will be later used in tests

--- src/AttendProbability.scala
+++ src/AttendProbability.scala
@@ -1,23 +1,34 @@
+class AttendProbability(personsP: List[Double]) {
+  val N = personsP.length
+
+  // precalculate the probabilities of attend/not-attend per person
+  // (we want fast lookup by index)
+  val pYes = personsP.toIndexedSeq
+  val pNo = personsP.map(1.0-).toIndexedSeq
+
+  // probability that a given set of persons attends
+  def P(att: Set[Int]) =
+    (0 until N).map { i =>
+      if (att contains i) pYes(i)
+      else pNo(i)
+    }.product
+
+  // probability of exact `n` attendees
+  def exact(n: Int) =
+    (0 until N).combinations(n).map(_.toSet).map(P).sum
+
+  // probability at least `n` attendees (=inclusive)
+  def atLeast(n: Int) =
+    (n to N).map(exact).sum
+
+  // probability less than `n` attendees (=exclusive)
+  def atMost(n: Int) =
+    (0 until n).map(exact).sum
+}
+
 object AttendProbability {
   def calculate(probabilities: List[Double], needed: Int): Double = {
-    val N = probabilities.length
-
-    // precalculate the probabilities of attend/not-attend per person
-    // (we want fast lookup by index)
-    val pYes = probabilities.toIndexedSeq
-    val pNo = probabilities.map(1.0-).toIndexedSeq
-
-    // probability that a given set of persons attends
-    def P(att: Set[Int]) =
-      (0 until N).map { i =>
-        if (att contains i) pYes(i)
-        else pNo(i)
-      }.product
-
-    // from `needed` up to `N` could be attending
-    (needed to N).map { attN =>
-      // attN is the exact number of attendees
-      (0 until N).combinations(attN).map(_.toSet).map(P).sum
-    }.sum
+    val probability = new AttendProbability(probabilities)
+    probability.atLeast(needed)
   }
 }
