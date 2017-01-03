4ae54b88e6de109f17422ed4f50bd2bf2bc3c64a
Alexander Azarov <azarov@osinka.ru>
2017-01-03 12:57:47 +0200

    Preconditions

--- src/AttendProbability.scala
+++ src/AttendProbability.scala
@@ -1,4 +1,7 @@
 class AttendProbability(personsP: List[Double]) {
+  require(personsP.nonEmpty, "The list of probabilities should not be empty")
+  require(personsP.forall {x => x>=0 && x<=1.0}, "The list of probabilities should contain values [0,1]")
+
   val N = personsP.length
 
   // precalculate the probabilities of attend/not-attend per person
@@ -14,16 +17,23 @@ class AttendProbability(personsP: List[Double]) {
     }.product
 
   // probability of exact `n` attendees
-  def exact(n: Int) =
-    (0 until N).combinations(n).map(_.toSet).map(P).sum
+  def exact(n: Int) = {
+    require(n >= 0 && n <= N, "`needed` should be from 0 to the number of probabilities")
+    if (n == 0) P(Set.empty)
+    else (0 until N).combinations(n).map(_.toSet).map(P).sum
+  }
 
   // probability at least `n` attendees (=inclusive)
-  def atLeast(n: Int) =
+  def atLeast(n: Int) = {
+    require(n >= 0 && n <= N, "`needed` should be from 0 to the number of probabilities")
     (n to N).map(exact).sum
+  }
 
   // probability less than `n` attendees (=exclusive)
-  def atMost(n: Int) =
+  def atMost(n: Int) = {
+    require(n >= 0 && n <= N, "`needed` should be from 0 to the number of probabilities")
     (0 until n).map(exact).sum
+  }
 }
 
 object AttendProbability {
